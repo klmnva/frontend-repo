86a329528e67f3ce682eef5ec40ccf18a9999666
Monse54 <bachansahab987@gmail.com>
2025-06-25 13:03:32 +0530

    color changed

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>User Management App</h1>
+        <h1>Hobby Tracker</h1>
         
         {/* User Form */}
         <div className="form-container">
